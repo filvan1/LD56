5847a45fd8fde908e0e6c645a0f4657fc4228a16
filvan1 <filip.vannfalt@gmail.com>
2024-10-06 20:38:34 +0200

    beetle skeetle

--- scripts/beetle.gd
+++ scripts/beetle.gd
@@ -3,6 +3,10 @@ class_name Beetle extends "enemy.gd"
 enum AttackingStates {NONE, WINDUP, CHARGING}
 enum MoveStates {NONE, TURNING, WALKING}
 
+@onready var sprite = $AnimatedSprite2D
+@onready var dash_particles_l = $DashParticlesL
+@onready var dash_particles_r = $DashParticlesR
+
 var current_attack_state = AttackingStates.NONE
 var windup_time = 2.0
 var charge_time = 0.5
@@ -25,13 +29,16 @@ var move_dir = rand.randi_range(-1, 1)
 signal on_charge
 
 func _ready() -> void:
-	aggro_range = 200.0
+	aggro_range = 100.0
 	activity_time = 1.0
 	CurrentState = EnemyState.IDLE
 	NextState = EnemyState.IDLE
+	sprite.play("idle")
+	sprite.speed_scale = 1
 
 func _physics_process(delta: float) -> void:
 	if not alive:
+		sprite.speed_scale = 0
 		return
 	
 	match(EnemyState):
@@ -100,26 +107,33 @@ func _process(delta: float) -> void:
 		EnemyState.ATTACKING:
 			match(current_attack_state):
 				AttackingStates.WINDUP:
+					sprite.speed_scale = 0.5
 					attack_counter += delta
 					target_angle_diff = rotate_towards_point(player.get_control_position(), delta * 2.0)
 					var target_dir = (player.get_control_position() - global_position).normalized()
-					global_position = get_bezier_position(attack_start_pos, attack_start_pos - target_dir, attack_start_pos - 5.0 * target_dir, attack_counter / 2)
+					
+					velocity = -Vector2(5.0, 0).rotated(target_dir.angle())
+					move_and_slide()
 					if(target_angle_diff < 0.01 && attack_counter > 2.0):
 						attack_counter = 0
 						attack_start_pos = global_position
 						attack_target_pos = global_position + (player.get_control_position() - global_position).normalized() * 50.0
 						current_attack_state = AttackingStates.CHARGING
+						
+						dash_particles_l.emitting = true
+						dash_particles_r.emitting = true
 						on_charge.emit()
 						
 				
 				AttackingStates.CHARGING:
+						
+					sprite.speed_scale = 2
 					attack_counter += delta
-					velocity = Vector2(50.0, 0).rotated(rotation)
-					move_and_slide() 
+					velocity = Vector2(80.0, 0).rotated(rotation)
+					move_and_slide()
 					#get_bezier_position(attack_start_pos, attack_target_pos, attack_target_pos, attack_counter)
-					
-					if(attack_counter > 1.0):
-						print(rotation)
+
+					if(attack_counter > 0.5):
 						attack_counter = 0
 						current_attack_state = AttackingStates.NONE
 						CurrentState = EnemyState.IDLE
@@ -133,10 +147,11 @@ func _process(delta: float) -> void:
 				EnemyState.MOVING:
 					if((player.get_control_position() - global_position).length() < aggro_range):
 						NextState = EnemyState.ATTACKING
-						print("attacking")
+						
+						sprite.play("move")
 					else:
 						NextState = EnemyState.IDLE
-						print("idle")
+						sprite.play("idle")
 					
 	
 				EnemyState.IDLE:
@@ -144,14 +159,14 @@ func _process(delta: float) -> void:
 						NextState = EnemyState.ATTACKING
 						current_attack_state = AttackingStates.WINDUP
 						attack_start_pos = global_position
-						print("attacking")
+						sprite.play("move")
 					
 					else:
 						NextState = EnemyState.MOVING
 						current_move_state = MoveStates.TURNING
 						
 						move_dir = rand.randi_range(-1, 1)
-						print("moving")
+						sprite.play("move")
 					
 						
 			
