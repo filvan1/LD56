a9f56e8982516f6cca0b0016b08a214e945d5aef
filvan1 <filip.vannfalt@gmail.com>
2024-10-06 12:16:42 +0200

    added speedy sound for dungie

--- scripts/dungie.gd
+++ scripts/dungie.gd
@@ -10,8 +10,11 @@ signal on_crash
 
 func _ready() -> void:
 	velocity = Vector2(-patrol_speed, 0)
+	CurrentState = EnemyState.IDLE
+	NextState = EnemyState.IDLE
 
 func _physics_process(delta: float) -> void:
+	change_state()
 	if (player.get_control_position() - global_position).length() < 128:
 		velocity += (player.get_control_position() - global_position) * delta * 0.15
 		velocity += velocity.normalized() * 10 * delta
@@ -20,19 +23,34 @@ func _physics_process(delta: float) -> void:
 		velocity += (get_room_center() - global_position) * delta * 0.15
 		velocity = velocity.limit_length(patrol_speed)
 	
+	
 	if move_and_slide():
 		velocity = Vector2.ZERO
 		position += (get_room_center() - global_position).normalized() * 10
 		on_crash.emit()
-
-	var v = get_real_velocity()
-	if v.length() > 1.0:
-		rotation = atan2(v.y, v.x)
-		sprite.play("roll")
-		sprite.speed_scale = velocity.length() / 50.0 * 2
+		NextState = EnemyState.IDLE
 	else:
-		sprite.speed_scale = 1.0
-		sprite.play("idle")
+		var v = get_real_velocity()
+		if v.length() > 1.0:
+		
+			rotation = atan2(v.y, v.x)
+			sprite.play("roll")
+			sprite.speed_scale = velocity.length() / 50.0 * 2
+			NextState = EnemyState.ATTACKING
+			$AudioPlayer.pitch_scale = velocity.length() / 100.0 * 2
+		else:
+			sprite.speed_scale = 1.0
+			sprite.play("idle")
+
+func change_state():
+	if CurrentState != NextState:
+		CurrentState = NextState
+		match(CurrentState):
+			EnemyState.ATTACKING:
+				$AudioPlayer.play()
+			EnemyState.IDLE:
+				$AudioPlayer.stop()
+				$AudioPlayer.pitch_scale = 0.01
 
 func _process(delta: float) -> void:
 	#super._process(delta)
