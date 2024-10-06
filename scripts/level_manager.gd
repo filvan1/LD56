142f689b88c717f0639be3860819c65b3b323fdd
Martin-Bergman <martin.je.bergman@gmail.com>
2024-10-06 15:39:15 +0200

    that's a wrap on map

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 var room_data = []
-var start_position
+var level_grid_size = 5
+var start_position: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -10,20 +11,32 @@ func _ready() -> void:
 	var layout_generator = get_node("LayoutGenerator")
 	var level_grid = layout_generator.get_layout()
 	var index = 0
+	var tiles
 	
+	var tilemap : TileMapLayer = get_node("TileMapLayer")
 	
 	for row in level_grid:
 		for room in row:
 			if room != null:
 				if room.contains("A"):
-					start_position = calculate_offset(index)
-				room_factory.generate(room, calculate_offset(index))
-			index +=1
+					start_position = calculate_offset(index) * 8
+				tiles = room_factory.generate(room)
+				var base = calculate_offset(index)
+				
+				for x in range(0, 8):
+					for y in range(0, 8):
+						var tile_index = tiles[x][y]
+						print(tile_index)
+						if tile_index != null:
+							tilemap.set_cell(base+ Vector2i(x,y), 0, Vector2i(tile_index % 10, tile_index / 10))
+			index += 1
 	print("start position: ")
 	print(start_position)
 	
 
 
 func calculate_offset(index):
-	return Vector2(128 * (index % 5), -128 * (index / 5))
+	return Vector2i(8 * (index % level_grid_size), 8 * (index / level_grid_size))
 	
+func get_start_position():
+	return start_position + Vector2(128, 128)
--- scripts/room_factory.gd
+++ scripts/room_factory.gd
@@ -4,8 +4,6 @@ var room_size = 8  # 8x8 tiles per room
 var tile_size = 32  # Each tile is 32x32 pixels
 var grid_width = 5  # Number of rooms across
 
-var Tilemap
-
 var TILE_CORNER_NW = [1]
 var TILE_WALL_N = [2, 3]
 var TILE_WALL_N_OPEN_L = [4]
@@ -26,7 +24,7 @@ var TILE_INTERSECTION_NW = [21]
 var TILE_INTERSECTION_NE = [22]
 var TILE_INTERSECTION_SW = [23]
 var TILE_INTERSECTION_SE = [24]
-var TILE_FLOOR = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 45, 36, 37, 38] 
+var TILE_FLOOR = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
 var TILE_PATH_TOP = [39, 40, 41, 42]
 var TILE_PATH_BOT = [43, 44, 45, 46]
 var TILE_PATH_LEFT = [47, 48, 49, 50]
@@ -59,56 +57,47 @@ const GRID_SIZE = 8
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	Tilemap = get_node("TileMapLayer")
-	
 	for x in range(GRID_SIZE):
 		var row = []
 		for y in range(GRID_SIZE):
 			row.append(null)
 		grid.append(row)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func generate(exits, offset):
+func generate(exits):
 	
 	build_walls(exits)
 	build_floor(exits)
-	print(exits)
 
-	return
+	return grid
 
 func build_walls(exits):
 	
 	#build corners
-	grid[0][0] = TILE_CORNER_NW
-	grid[GRID_SIZE - 1][0] = TILE_CORNER_NE
-	grid[0][GRID_SIZE -1] = TILE_CORNER_SW
-	grid[GRID_SIZE -1][GRID_SIZE -1] = TILE_CORNER_SE
+	grid[0][0] = get_tile(TILE_CORNER_NW)
+	grid[GRID_SIZE - 1][0] = get_tile(TILE_CORNER_NE)
+	grid[0][GRID_SIZE -1] = get_tile(TILE_CORNER_SW)
+	grid[GRID_SIZE -1][GRID_SIZE -1] = get_tile(TILE_CORNER_SE)
 	
 	#build walls
-	
-	for x in range(1, GRID_SIZE-2):
+	for x in range(1, GRID_SIZE-1):
 		grid[x][0] = get_tile(TILE_WALL_N)
 	if exits.contains("N"):
 		grid[3][0] = get_tile(TILE_WALL_N_OPEN_L)
 		grid[4][0] = get_tile(TILE_WALL_N_OPEN_R)
 		
-	for y in range(1, GRID_SIZE-2):
+	for y in range(1, GRID_SIZE-1):
 		grid[GRID_SIZE-1][y] = get_tile(TILE_WALL_E)
 	if exits.contains("E"):
 		grid[GRID_SIZE-1][3] = get_tile(TILE_WALL_E_OPEN_U)
 		grid[GRID_SIZE-1][4] = get_tile(TILE_WALL_E_OPEN_D)
 		
-	for x in range(1, GRID_SIZE-2):
+	for x in range(1, GRID_SIZE-1):
 		grid[x][GRID_SIZE-1] = get_tile(TILE_WALL_S)
 	if exits.contains("S"):
-		grid[3][GRID_SIZE-1] = get_tile(TILE_WALL_N_OPEN_L)
-		grid[4][GRID_SIZE-1] = get_tile(TILE_WALL_N_OPEN_R)
+		grid[3][GRID_SIZE-1] = get_tile(TILE_WALL_S_OPEN_R)
+		grid[4][GRID_SIZE-1] = get_tile(TILE_WALL_S_OPEN_L)
 		
-	for y in range(1, GRID_SIZE-2):
+	for y in range(1, GRID_SIZE-1):
 		grid[0][y] = get_tile(TILE_WALL_W)
 	if exits.contains("W"):
 		grid[0][3] = get_tile(TILE_WALL_W_OPEN_U)
@@ -117,8 +106,8 @@ func build_walls(exits):
 	
 func build_floor(exits):
 	
-	for x in range(1, GRID_SIZE-2):
-		for y in range(1, GRID_SIZE-2):
+	for x in range(1, GRID_SIZE-1):
+		for y in range(1, GRID_SIZE-1):
 			grid[x][y] = get_tile(TILE_FLOOR)
 		
 	
